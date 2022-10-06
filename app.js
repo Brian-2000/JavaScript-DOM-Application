95955717b52924a2d6252c754cf2cace9c41f3b0
brian-2000 <mucherebrian@gmail.com>
2022-10-06 11:02:24 +0300

    Javascript DOM Application

---
+++ app.js
@@ -0,0 +1,63 @@
+// 1. Change color of car and addToCart button color when a color is selected
+// - Selecting Elements
+const redColor = document.querySelector(".red");
+const grayColor = document.getElementsByClassName("gray");
+const blackColor = document.querySelector(".black");
+const whiteColor = document.querySelector(".white");
+const yellowColor = document.querySelector(".yellow");
+const cartButton = document.getElementById("button");
+const itemTag = document.getElementsByTagName("h3")[0];
+const imageCard = document.querySelector(".product-image");
+const feedbackBtn = document.querySelector(".feedback");
+
+// Modifying Elements
+// - Add Event Listeners
+// - Red Color
+redColor.addEventListener("click", function () {
+  cartButton.style.backgroundColor = "red";
+  itemTag.style.backgroundColor = "red";
+  imageCard.style.backgroundImage = 'url("./images/red3.jpg")';
+});
+
+// - Gray Color
+grayColor[0].addEventListener("click", function () {
+  cartButton.style.backgroundColor = "gray";
+  itemTag.style.backgroundColor = "gray";
+  imageCard.style.backgroundImage = 'url("./images/grey4.jpg")';
+});
+
+// - Black Color
+blackColor.addEventListener("click", function () {
+  cartButton.style.backgroundColor = "black";
+  itemTag.style.backgroundColor = "black";
+  imageCard.style.backgroundImage = 'url("./images/black1.jpg")';
+});
+
+// - white Color
+whiteColor.addEventListener("click", function () {
+  cartButton.style.backgroundColor = "white";
+  itemTag.style.backgroundColor = "white";
+  imageCard.style.backgroundImage = 'url("./images/white2.jpeg")';
+});
+
+// - Yellow Color
+yellowColor.addEventListener("click", function () {
+  cartButton.style.backgroundColor = "yellow";
+  itemTag.style.backgroundColor = "yellow";
+  imageCard.style.backgroundImage = 'url("./images/yellow.jpg")';
+});
+
+// Button Click Implementation
+// - Cart Button
+const cart = () => {
+  cartButton.style.display = "none";
+  feedbackBtn.style.display = "block";
+};
+cartButton.addEventListener("click", cart);
+
+// - Feedback Button
+const feedback = () => {
+  cartButton.style.display = "block";
+  feedbackBtn.style.display = "none";
+};
+feedbackBtn.addEventListener("click", feedback);
